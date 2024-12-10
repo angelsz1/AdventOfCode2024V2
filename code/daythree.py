2e79b4fcd4584bf859d7c23a757a40f92476e7d1
Angel Szust <angel.szust@avature.net>
2024-12-10 12:01:56 -0300

    daythree part two

--- code/daythree.py
+++ code/daythree.py
@@ -17,6 +17,52 @@ class DayThree(Solution):
         return sum
 
     def part_two(self):
-        pass
+        sum = 0
+        dos = re.finditer(r'do\(\)', self.input)
+        donts = re.finditer(r'don\'t\(\)', self.input)
+        muls = re.finditer(r'mul\(\d+,\d+\)', self.input)
+        dosStart, dontsStart = self.get_do_donts(dos, donts)
+        starts, isValid = self.get_starts(dosStart, dontsStart)
+        for mul in muls:
+            if self.is_valid_mul(mul.start(), starts, isValid):
+                value = mul.group()
+                nums = [int(val) for val in re.split(',', re.findall(r'\d+,\d+', value)[0])]
+                sum += nums[0] * nums[1]
+        return sum
+
+    def is_valid_mul(self, mul, starts, valid):
+        i = 0
+        while i < len(starts) and mul > starts[i]:
+            i += 1
+        return valid[i-1]
+        
+    def get_starts(self, do, dont):
+        starts = [0]
+        valid = [True]
+        while len(do) > 0 and len(dont) > 0:
+            if do[0] < dont[0]:
+                starts.append(do.pop(0))
+                valid.append(True)
+            else:
+                starts.append(dont.pop(0))
+                valid.append(False)
+        for d in do:
+            starts.append(d)
+            valid.append(True)
+        for d in dont:
+            starts.append(d)
+            valid.append(False)
+        return starts, valid
+
+
+    def get_do_donts(self, dos, donts):
+        dosStart = []
+        dontsStart = []
+        for do in dos:
+            dosStart.append(do.start())
+        for dont in donts:
+            dontsStart.append(dont.start())
+        return dosStart, dontsStart
+
 
 DayThree().run(sys.argv[1])
